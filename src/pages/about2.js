6350e4bc03d75ba592e23050c7e7f3340ad75f7f
Fabian Tan <fabiantanberlin@gmail.com>
2020-04-26 13:54:25 +0200

    adding about2 page

---
+++ src/pages/about2.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class NotFoundPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="About2 Page" />
+        <h1>About 2</h1>
+        <p>This is the about2 page.</p>
+      </Layout>
+    )
+  }
+}
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
