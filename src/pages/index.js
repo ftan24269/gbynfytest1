2b153841da9cfab10f5d8fdadfd0e407bb2b5707
Fabian Tan <fabiantanberlin@gmail.com>
2020-04-26 13:54:50 +0200

    added link to about2

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,6 +31,9 @@ class IndexPage extends React.Component {
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
+        <Link to="/about2/">
+          <Button marginTop="35px">Go to About2</Button>
+        </Link>
       </Layout>
     )
   }
